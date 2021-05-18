7779f4928df41a8a66ce78b008b5cd2bd3cb2569
mmg36 <mehdi.goudarzi@oodlefinance.com>
2021-05-19 00:03:26 +0100

    First website layout

--- js/basic-function.js
+++
@@ -1,7 +0,0 @@
-var msg = 'Sign upo to recieve our !';
-function updateMessage() {
-  var el = document.getElementById('message');
-  el.textContent = msg;
-}
-
-updateMessage();
---
+++ js/dom.js
@@ -0,0 +1,8 @@
+// console.dir(document);
+
+console.log(document.title);
+var headerTitle = document.getElementById(
+  document.getElementById('header-title')
+);
+console.log(headerTitle);
+headerTitle.innerText = 'Hello';
